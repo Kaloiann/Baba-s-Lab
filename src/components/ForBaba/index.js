c30fe51948122a70945f291ad96e3b2a5b231517
Kaloiann <kaloain.cenkulovski_5@abv.bg>
2022-03-17 15:43:24 +0200

    text & css changes

--- src/components/ForBaba/index.js
+++ src/components/ForBaba/index.js
@@ -7,7 +7,7 @@ const ForBaba = () => {
     <div className={css.container}>
       <h1>For Baba !</h1>
       <p>The purpose of this site is to show my general knowledge of React, JavaScript, CSS and HTML.
-        Here you can see how I use features of every programming language and use a lot of new skills to me.
+        Here you can see how I use features of every programming language and how I apply many skills that are new to me.
       </p>
     </div>
   )
