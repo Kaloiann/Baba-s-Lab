f4064470e49b95c6b776ab6ff725701b39357728
Kaloiann <kaloain.cenkulovski_5@abv.bg>
2022-03-19 16:39:18 +0200

    finished

--- src/components/Navigation/SubMenuMobile/index.js
+++ src/components/Navigation/SubMenuMobile/index.js
@@ -21,61 +21,7 @@ export const SubMenuMobile = ({ setShowMenu }) => {
     setShowMenu(false)
     history.push(`/meals/subcategory/${subCat}`)
   }
-  // - decomposition
-  // - composition
-
-  // (issue)
-  // #1 define - we need mobile submenu
-  // #2 wrspm - we have desktop submenu
-  // #3 decompose
-
-  // branch: mobile-submenu
-  
-  // copy this component and use it in <Mobile />
-  
-  // category items use onClick instead of onMouseEnter
-  
-  // on category click, hide categories and show subcategories ONLY
-  
-  // add back button in subcategories menu
-  
-  // add "view all" in subcategories menu, which will open the category link
-  
-  // on subcategory click, redirect
-  
-  // on redirect, hide menu
-  
-  // #4 acceptence criteria - define when a step is completed
-  //  - write test
-  //   are categories hided?
-  //   are subcategories shown?
-  //  - find corner case
-
-  //  register form
-  //  email
-  //  confirm email
-  //  password
-   // happy path
-   // unhappy path
-   // test if email and confirm email are the same
-   // test if theere is password
-
-  // #5 composition
-  //  - paradigms
-  //  - design patterns
-  //  - libraries
-
-
-  // google:
-  //  software engineering decomposition and composition
-  //  software engineering user stories and acceptance criterias
-  //  software engineering wrspm
-  //  software engineering happy path and unhappy path
-  
-  
-  // create merge request from gitHub
-  // git checkout main
-  // git pull
+ 
 
   return (
     <div className={css['main-category']}>
